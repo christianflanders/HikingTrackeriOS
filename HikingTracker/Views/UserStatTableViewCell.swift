9671de74b973b4aed530fac7b0512a6ee8fc5758
Christian Flanders <christian@christianflanders.com>
2017-12-28 13:38:29 -0800

    Added new user Info view controller

---
+++ HikingTracker/Views/UserStatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserStatTableViewCell.swift
+//  HikingTracker
+//
+//  Created by Christian Flanders on 12/28/17.
+//  Copyright © 2017 Christian Flanders. All rights reserved.
+//
+
+import UIKit
+
+class UserStatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
