a9e7463d7fd3a1ca572761e6ea725375399b3fe5
Christian Flanders <christian@christianflanders.com>
2017-12-29 14:30:14 -0800

    Made it easier to set values for user information

--- HikingTracker/Model/UserModel.swift
+++ HikingTracker/Model/UserModel.swift
@@ -9,31 +9,94 @@
 import Foundation
 
 class User {
-
+    
+    private let defaults = UserDefaults.standard
+    private let keys = UserEntriesKeys()
     
     var weightInKilos: Double? {
-        let defaults = UserDefaults.standard
-        return defaults.double(forKey: "weight")
+        get {
+            return defaults.double(forKey: keys.weight)
+        }
+        set {
+            defaults.set(newValue, forKey: keys.weight)
+        }
+        
     }
     var heightInMeters: Double? {
-        let defaults = UserDefaults.standard
-        return defaults.double(forKey: "height")
-    }
-    var name: String? {
-        let defaults = UserDefaults.standard
-        if let returnName = defaults.string(forKey: "name") {
-            return returnName
-        } else {
-            return nil
+        get {
+            return defaults.double(forKey: keys.height)
+        }
+        set {
+            defaults.set(newValue, forKey: keys.height)
         }
         
     }
     
-    var userDisplayUnits: DisplayUnits = .freedomUnits
-
+    var birthdate: Date? {
+        get {
+            return defaults.object(forKey: keys.birthdate) as! Date
+        }
+        set {
+            defaults.set(newValue, forKey: keys.birthdate)
+        }
+    }
+    
+    var gender: String? {
+        get {
+            return defaults.string(forKey: keys.gender)
+        }
+        set {
+            defaults.set(newValue, forKey: keys.gender)
+        }
+    }
+    
+    var name: String? {
+        get {
+            if let returnName = defaults.string(forKey: keys.name) {
+                return returnName
+            } else {
+                return nil
+            }
+        }
+        set {
+            defaults.set(newValue, forKey: keys.name)
+        }
+    }
+    
+    var userDisplayUnits: DisplayUnits {
+        get {
+            let fromDefaults = defaults.object(forKey: keys.localUnits) as! String
+            switch fromDefaults {
+            case "freedomUnits":
+                return .freedomUnits
+            case "metric" :
+                return .metric
+            default:
+                print("Unknown value stored in user defaults for display units")
+                return .freedomUnits
+            }
+        }
+        set {
+            switch newValue {
+            case .freedomUnits:
+                defaults.set("freedomUnits", forKey: keys.localUnits)
+            case .metric:
+                defaults.set("metric", forKey: keys.localUnits)
+            }
+        }
+    }
 }
 
 enum DisplayUnits {
     case metric
     case freedomUnits
+}
+
+struct UserEntriesKeys {
+    let weight = "weight"
+    let height = "height"
+    let name = "name"
+    let birthdate = "birthdate"
+    let gender = "gender"
+    let localUnits = "localUnits"
 }
