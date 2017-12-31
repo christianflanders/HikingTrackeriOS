4f94850eb77367223edcfbd254d9d31c7a5b166f
Christian Flanders <christian@christianflanders.com>
2017-12-31 13:51:27 -0800

    Refactoring into multiple storyboards

--- HikingTracker/Views/UserStatTableViewCell.swift
+++ HikingTracker/Views/UserStatTableViewCell.swift
@@ -10,10 +10,6 @@ import UIKit
 
 class UserStatTableViewCell: UITableViewCell {
     
-    
-    @IBOutlet weak var userStatDescriptionLabel: UILabel!
-    @IBOutlet weak var userStatEnterLabel: UILabel!
-    
 
     override func awakeFromNib() {
         super.awakeFromNib()
