0ca0f2dd71817acb84b08310408d541edc4ffabe
Christian Flanders <christian@christianflanders.com>
2017-12-31 13:29:13 -0800

    Added user information container to make entering user data cleaner

---
+++ HikingTracker/Model/UserInformationValues.swift
@@ -0,0 +1,20 @@
+//
+//  UserInformationValues.swift
+//  HikingTracker
+//
+//  Created by Christian Flanders on 12/31/17.
+//  Copyright © 2017 Christian Flanders. All rights reserved.
+//
+
+import Foundation
+
+struct UserInformationValues {
+    
+    var heightInMeters: Double?
+    var gender: String?
+    var name: String?
+    var weightInKG: Double?
+    var birthdate: Date?
+    
+    
+}
