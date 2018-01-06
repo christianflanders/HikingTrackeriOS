6a6a11bc04874f8c529adad6fe048eae70c0b0be
Christian Flanders <christian@christianflanders.com>
2018-01-06 14:04:49 -0800

    Initial commit

---
+++ HikingTracker/Model/PauseHikeProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  PauseHikeProtocol.swift
+//  HikingTracker
+//
+//  Created by Christian Flanders on 1/6/18.
+//  Copyright © 2018 Christian Flanders. All rights reserved.
+//
+
+import Foundation
+
+protocol PauseHikeDelegate {
+    func pauseHike()
+    func resumeHike()
+}
