2f62e89b9826fa87290980fd5cc1fff2246fcef7
Christian Flanders <christian@christianflanders.com>
2018-01-07 15:25:30 -0800

    Added Hike Information Protocol

---
+++ HikingTracker/Model/Hike Information Protocol.swift
@@ -0,0 +1,9 @@
+//
+//  Hike Information Protocol.swift
+//  HikingTracker
+//
+//  Created by Christian Flanders on 1/7/18.
+//  Copyright © 2018 Christian Flanders. All rights reserved.
+//
+
+import Foundation
