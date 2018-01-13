ecb64d09c65b0488073b0ae11e074efb4e95c108
Christian Flanders <christian@christianflanders.com>
2018-01-13 13:03:30 -0800

    Added skeleton for settings screen

--- HikingTracker/ViewControllers/SettingsTableViewController.swift
+++ HikingTracker/ViewControllers/SettingsTableViewController.swift
@@ -10,86 +10,54 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // MARK: Enums
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+    // MARK: Constants
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+
+    // MARK: Variables
+
+
+    // MARK: Outlets
+    @IBOutlet weak var unitsSegmentedControl: UISegmentedControl!
+
+    @IBOutlet weak var pauseSoundSwitchOutlet: UISwitch!
+    @IBOutlet weak var resetChecklistSwitch: UISwitch!
+    @IBOutlet weak var openWatchAppAutomaticallySwitch: UISwitch!
+
+    // MARK: Weak Vars
+
+
+    // MARK: Public Variables
+
+
+    // MARK: Private Variables
+
+
+    // MARK: View Life Cycle
+
+
+    // MARK: IBActions
+
+    @IBAction func unitsSegmentedControlPressed(_ sender: UISegmentedControl) {
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+    @IBAction func pauseSoundSwitchValueChanged(_ sender: UISwitch) {
 
     }
-    */
+    @IBAction func resetChecklistSwitchValueChanged(_ sender: UISwitch) {
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
     }
-    */
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func openWatchAppAutomaticallySwitchValueChanged(_ sender: UISwitch) {
     }
-    */
+
 
 }
+
+
+
+
+
+
