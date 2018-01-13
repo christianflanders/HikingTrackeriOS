d8930da8905ccb96d40a9b6fd5724f2ec2d5c1b7
Christian Flanders <christian@christianflanders.com>
2018-01-13 14:13:26 -0800

    started checklist

---
+++ HikingTracker/Views/ChecklistTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChecklistTableViewCell.swift
+//  HikingTracker
+//
+//  Created by Christian Flanders on 1/13/18.
+//  Copyright © 2018 Christian Flanders. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
