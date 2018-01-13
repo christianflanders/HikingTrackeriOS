073fec1a54c72ad2093ae7ab4718ee0a3b9d25c1
Christian Flanders <christian@christianflanders.com>
2018-01-12 18:11:08 -0800

    Updating icons

---
+++ HikingTracker/Model/Hike/Save Hike To HealthKit.swift
@@ -0,0 +1,9 @@
+//
+//  Save Hike To HealthKit.swift
+//  HikingTracker
+//
+//  Created by Christian Flanders on 1/12/18.
+//  Copyright © 2018 Christian Flanders. All rights reserved.
+//
+
+import Foundation
