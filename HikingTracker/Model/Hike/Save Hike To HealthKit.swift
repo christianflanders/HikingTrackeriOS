0e5adb31e52a50d0510da5924f0510bbe21a2605
Christian Flanders <christian@christianflanders.com>
2018-01-12 18:12:28 -0800

    Adding save hike to health kit

--- HikingTracker/Model/Hike/Save Hike To HealthKit.swift
+++ HikingTracker/Model/Hike/Save Hike To HealthKit.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import HealthKit
+
+extension HikeInProgress {
+    func storeHikeToHealthKit(name: String)  {
+        
+        let healthStore = HKHealthStore()
+        
+        let caloriesBurned = self.caloriesBurned
+        let calorieUnit = HKUnit(from: .kilocalorie)
+        let hkCalories = HKQuantity(unit: calorieUnit, doubleValue: caloriesBurned)
+        
+        let distanceInMeters = self.totalDistanceInMeters
+        let distanceUnit = HKUnit(from: .meter)
+        let hkDistance = HKQuantity(unit: distanceUnit, doubleValue: distanceInMeters)
+        
+        guard let startDate = self.startDate else {
+            print("Problem getting start date")
+            return
+        }
+        guard let endDate = self.endDate else {
+            print("Problem getting start date")
+            return
+        }
+        
+        let duration = self.durationInSeconds
+        
+        let workout = HKWorkout(activityType: .hiking, start: startDate, end: endDate, duration: duration,
+                                totalEnergyBurned: hkCalories, totalDistance: hkDistance, device: HKDevice.local(), metadata: nil)
+        
+        healthStore.save(workout) { (_, error) in
+            if error == nil {
+                print("Success saving workout to health kit store!")
+            } else {
+            }
+        }
+    }
+}
+
