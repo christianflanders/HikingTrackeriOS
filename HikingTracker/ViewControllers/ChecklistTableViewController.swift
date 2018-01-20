9f7e7bc621e27bac26da2dbe565f9d6ddc768d80
Christian Flanders <christian@christianflanders.com>
2018-01-20 12:54:16 -0800

    Checklist done and working.

--- HikingTracker/ViewControllers/ChecklistTableViewController.swift
+++ HikingTracker/ViewControllers/ChecklistTableViewController.swift
@@ -7,87 +7,175 @@
 //
 
 import UIKit
+import M13Checkbox
+import Firebase
 
-class ChecklistTableViewController: UITableViewController {
+class ChecklistTableViewController: UITableViewController, ChecklistButtonFlippedDelegate {
+
+
+    // MARK: Enums
+
+    // MARK: Constants
+    var checklistItemOne = ChecklistItem(name: "Backpack", checked: false, dateAdded: Date())
+    var checklistItemTwo = ChecklistItem(name: "Leash", checked: true, dateAdded: Date())
+    var checklistItemThree = ChecklistItem(name: "Water", checked: false, dateAdded: Date())
+
+
+    var checklistItems = [ChecklistItem]()
+
+    // MARK: Variables
+
+
+    // MARK: Outlets
+
+    @IBOutlet var addToChecklistView: AddItemToChecklistView!
+
+    // MARK: Weak Vars
+
+
+    // MARK: Public Variables
+    var ref: DatabaseReference!
+    var handle: DatabaseHandle!
+
+    // MARK: Private Variables
+
+
+    // MARK: View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        checklistItems.append(checklistItemOne)
+        checklistItems.append(checklistItemTwo)
+        checklistItems.append(checklistItemThree)
+        tableView.allowsSelection = false
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        ref = Database.database().reference()
+        let userUID = Auth.auth().currentUser?.uid
+        handle = ref.child(userUID!).child("Checklist").observe(.childAdded, with: { (snapshot) in
+            let checkListDict = snapshot.value as? [String:Any] ?? [:]
+            let name = checkListDict["Name"] as! String
+            let checked = checkListDict["Checked"] as! Bool
+            let date = checkListDict["Date"] as! String
+            let dateFromString = self.convertStringDateToDate(string: date)
+            var newChecklistItem = ChecklistItem(name: name, checked: checked, dateAdded: dateFromString!)
+            self.checklistItems.append(newChecklistItem)
+            self.tableView.reloadData()
+        })
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
+    // MARK: IBActions
+
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 10
+        return checklistItems.count
+
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistCell", for: indexPath) as! ChecklistTableViewCell
-
-        // Configure the cell...
+        cell.index = indexPath.row
+        cell.delegate = self
+        cell.checklistItemToDisplay = checklistItems[indexPath.row]
+        cell.setUpCell()
 
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
-    // Override to support editing the table view.
+
+    // MARK: Table View Editing
+
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+    // MARK: ChecklistButtonFlipped Delegate
 
+    func checklistButtonFlipped(index: Int) {
+        print(index)
+        var selectedItem = checklistItems[index]
+        var checked = selectedItem.checked
+        if checked {
+            selectedItem.checked = false
+        } else {
+            selectedItem.checked = true
+        }
+        changeFirebaseCheckedValueForItem(selectedItem,checked: selectedItem.checked)
+        print(selectedItem.name)
+        print(selectedItem.checked)
     }
-    */
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    func changeFirebaseCheckedValueForItem(_ item: ChecklistItem, checked: Bool) {
+        let databaseRef = Database.database().reference()
+        if let userUID = Auth.auth().currentUser?.uid {
+            databaseRef.child(userUID).child("Checklist").child(item.name).child("Checked").setValue(checked)
+        } else {
+            print("Problem getting userUID")
+        }
     }
-    */
 
-    /*
-    // MARK: - Navigation
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add", message: "Add a checklist item", preferredStyle: .alert)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        alert.addTextField { (textField) in
+            textField.text = "Add Item"
+        }
+
+        // 3. Grab the value from the text field, and print it when the user clicks OK.
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            let textField = alert!.textFields![0]
+            print("Text field: \(textField.text)")
+            guard let text = textField.text else {return}
+            let newChecklistItem = ChecklistItem(name: text, checked: false, dateAdded: Date())
+//            self.checklistItems.append(newChecklistItem)
+            self.uploadChecklistItemToFirebase(newChecklistItem)
+            self.tableView.reloadData()
+        }))
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
     }
-    */
 
+
+    private func convertStringDateToDate(string: String) -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .short
+        let dateFromString = dateFormatter.date(from: string)
+        return dateFromString
+    }
+
+    private func uploadChecklistItemToFirebase(_ item: ChecklistItem) {
+        var fireBaseDict = [String: Any]()
+         let stringDate = item.dateAdded.displayString
+            fireBaseDict["Date"] = stringDate
+
+         let name = item.name
+            fireBaseDict["Name"] = name
+
+         let checked = item.checked
+            fireBaseDict["Checked"] = checked
+
+
+        let databaseRef = Database.database().reference()
+        if let userUID = Auth.auth().currentUser?.uid {
+            databaseRef.child(userUID).child("Checklist").child(name).setValue(fireBaseDict)
+        } else {
+            print("Problem getting userUID")
+        }
+    }
 }
