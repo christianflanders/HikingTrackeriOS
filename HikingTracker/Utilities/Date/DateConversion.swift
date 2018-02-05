dce6aa2edca45bbc1039d242bc6072fedf61f3bd
Christian Flanders <christian@christianflanders.com>
2018-02-05 10:04:55 -0800

    Always check for a built in function before writing your own -_-

--- HikingTracker/Utilities/Date/DateConversion.swift
+++ HikingTracker/Utilities/Date/DateConversion.swift
@@ -9,35 +9,15 @@
 import Foundation
 
 class DateHelper {
+
+
     
     func convertDurationToStringDate(_ duration: Double) -> String {
-        let seconds = Int(duration)
-        
-        var stringMinutes = ""
-        var stringSeconds = ""
-        var stringHours = ""
-        let calculatedSeconds = seconds % 60
-        if calculatedSeconds < 10 {
-            stringSeconds = String("0\(calculatedSeconds)")
-        } else {
-            stringSeconds = String(calculatedSeconds)
-        }
-        let calculatedMinutes = seconds / 60
-        if calculatedMinutes == 0 {
-            stringMinutes = "00"
-        }
-        if calculatedMinutes < 10 {
-            stringMinutes = String("0\(calculatedMinutes % 60)")
-        } else {
-            stringMinutes = String(calculatedMinutes % 60)
-        }
-        let calculatedHours = calculatedMinutes / 60
-        if calculatedHours < 10 {
-            stringHours = String("0\(calculatedHours)")
-        } else {
-            stringHours = String(calculatedHours)
-        }
-        let calculatedDurationString = "\(stringHours):\(stringMinutes):\(stringSeconds)"
-        return calculatedDurationString
+        let timeFormattter = DateComponentsFormatter()
+        timeFormattter.unitsStyle = .positional
+        timeFormattter.allowedUnits = [.hour, .minute, .second]
+        timeFormattter.zeroFormattingBehavior = .pad
+        guard let formattedDuration = timeFormattter.string(from:duration ) else { return "-" }
+        return formattedDuration
     }
 }
